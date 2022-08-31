f3c297658d9c1f3c8a99976314f336cac47c6483
Alex Karnakov <ak2005@yandex.ru>
2022-08-31 18:02:22 +0300

    Modify IfElse

--- Example006_Base/Program.cs
+++ Example006_Base/Program.cs
@@ -7,6 +7,10 @@ int e = 4;
 int max = a;
 
 if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
 
-Console.Write("max - ");
+Console.Write("max = ");
 Console.WriteLine(max);
