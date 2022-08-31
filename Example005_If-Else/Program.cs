064db3eb0fe9de52476d0b622263aa5cad50fdc1
Alex Karnakov <ak2005@yandex.ru>
2022-08-31 17:42:09 +0300

    Add If-Else

---
+++ Example005_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Have a nice day, Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
